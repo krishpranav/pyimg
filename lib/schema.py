4e524becdd634ba7402faeca96e65e59d9bf52cc
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-05-06 17:03:15 +0530

    schema

--- lib/schema.py
+++ lib/schema.py
@@ -31,3 +31,43 @@ logger = logging.getLogger(__name__)
 
 class InvalidUrlError(ValueError):
     pass
+
+class LazyLoadingImage:
+    def __init__(
+        self,
+        *,
+        filepath: str | None = None,
+        url: str | None = None,
+        img: "Image.Image | None" = None,
+        b64: str | None = None,
+    ):
+        if not filepath and not url and not img and not b64:
+            msg = "You must specify a url or filepath or img or base64 string"
+            raise ValueError(msg)
+        
+        if sum([bool(filepath), bool(url), bool(img), bool(u64)]) > 1:
+            raise ValueError("You cannot multiple input methods")
+        
+        if filepath and not os.path.exists(filepath):
+            msg = f"File does not exists: {filepath}"
+            raise FileNotFoundError(msg)
+        
+        if url:
+            from urllib3.exceptions import LocationValueError
+            from urllib3.util import parse_url
+            
+            try:
+                parsed_url = parse_url(url)
+            except LocationValueError:
+                raise InvalidUrlError(f"Invalid url: {url}")
+            if parsed_url.scheme not in {"http", "https"} or not parsed_url.host:
+                msg = f"Invalid url: {url}"
+                
+        if b64:
+            img = self.load_image_from_base64(b64)
+        
+        self._lazy_filepath = filepath
+        self._lazy_url = url
+        self._img = img
+
+    
