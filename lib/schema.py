60f9af476dedeb3e17c666f42726cdb3360b2f3b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-05-07 16:21:38 +0530

    lib: schema

--- lib/schema.py
+++ lib/schema.py
@@ -70,4 +70,102 @@ class LazyLoadingImage:
         self._lazy_url = url
         self._img = img
 
+    def __getattr__(self, key):
+        if key == "_img":
+            raise AttributeError()
+        self._load_img()
+        return getattr(self._img, key)
     
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+    
+    def _load_img(self):
+        if self._img is None:
+            from PIL import Image, ImageOps
+            
+            if self._lazy_filepath:
+                self._img = Image.open(self._lazy_filepath)
+                logger.debug(
+                    f"Loaded input 🖼  of size {self._img.size} from {self._lazy_filepath}"
+                )
+            elif self._lazy_url:
+                import requests
+                
+                self._img = Image.open(
+                    BytesIO(
+                        requests.get(self._lazy_url, stream=True, timeout=60).content
+                    )
+                )
+                logger.debug(
+                    f"Loaded input 🖼  of size {self._img.size} from {self._lazy_filepath}"
+                )
+            else:
+                raise ValueError("You must specify a url or filepath")
+
+            self._img = ImageOps.exif_transpose(self._img)
+    
+    @classmethod
+    def __get_pydantic_core_schema(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> core_schema.CoreSchema:
+        def validate(value: Any) -> "LazyLoadingImage":
+            from PIL import Image, UnidentifiedImageError
+            
+            if isinstance(value, cls):
+                return value
+            if isinstance(value, Image.Image):
+                return cls(img=value)
+            if isinstance(value, str):
+                if "." in value [:1000]:
+                    try:
+                        return cls(filepath=value)
+                    except FileNotFoundError as e:
+                        raise ValueError(str(e))
+                try:
+                    return cls(b64=value)
+                except UnidentifiedImageError:
+                    msg = "base64 string was not recognized as a valid image type"
+            
+            if isinstance(value, dict):
+                return cls(**value)
+            
+            msg = "Image value must be either a LazyLoadingImage, PIL.Image.Image or Base64 string"
+            raise ValueError(msg)
+
+        def handle_b64(value: Any) -> "LazyLoadingImage":
+            if isinstance(value, str):
+                return cls(b64=value)
+            
+            msg = "Image value must be either a LazyLoadingImage, PIL.Image.Image or a Base64 string"
+            raise ValueError(msg)
+            
+            return core_schema.json_or_python_schema(
+                json_schema=core_schema.chain_schema(
+                    [
+                        core_schema.str_schema(),
+                        core_schema.no_info_before_validator_function(
+                            handle_b64, core_schema.any_schema()
+                        ),
+                    ]
+                ),
+                python_schema=core_schema.no_info_before_validator_function(
+                    validate, core_schema.any_schema()
+                ),
+                serialization=core_schema.plain_serializer_function_ser_schema(str),
+            ) 
+        
+        @staticmethod
+        def save_image_as_base64(image: "Image.Image") -> str:
+            buffered = io.BytesIO()
+            image.save(buffered, format="PNG")
+            img_bytes = buffered.getvalue()
+            return base64.b64encode(img_bytes).decode()
+        
+        @staticmethod
+        def load_image_from_base64(image_str: str) -> "Image.Image":
+            from PIL import Image
+            
+            img_bytes = base64.b64decode(image_str)
+            return Image.open(io.BytesIO(img_bytes))
+        
+        
